a4831b71541631a558afcf913d9e4281eeacd19a
AvirukBasak <avirukbasak@gmail.com>
2023-08-16 11:26:39 +0530

    added tic-tac-toe

--- C++/tic-tac-toe-game/main.cpp
+++ C++/tic-tac-toe-game/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <string>
 
 class Game
 {
-public:
-    /** Possible player states */
     typedef enum {
         PLAYER_X,
-        PLAYER_O
+        PLAYER_O,
+        NONE
     } Player;
 
-    /** Input and verify player value */
+    Player board[3][3];
+    Player now;
+    Player winner;
+public:
     static Player inputPlayer(std::string prompt)
     {
         std::cout << prompt;
@@ -24,53 +27,152 @@ public:
         return PLAYER_X;
     }
 
-    Game(Player p1, Player p2)
+    Game(Player p1, Player p2) : winner(NONE)
     {
-
+        if (p1 == p2) {
+            std::cout << "Invalid game, both players are same\n";
+            exit(1);
+        }
+        if (p1 == PLAYER_X) now = p1;
+        else now = p2;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                board[i][j] = NONE;
+            }
+        }
     }
 
-    void inputCell()
+    void displayBoard()
     {
-
+        std::cout << "Current board:\n";
+        std::cout << "   - 1 - 2 - 3 -\n";
+        for (int i = 0; i < 3; ++i) {
+            std::cout << " " << (i+1) << " | ";
+            for (int j = 0; j < 3; ++j) {
+                char symbol = (board[i][j] == PLAYER_X) ?
+                    'X' : (board[i][j] == NONE) ?
+                        ' ' : 'O';
+                std::cout << symbol << " | ";
+            }
+            std::cout << "\n";
+        }
+        std::cout << "\n";
     }
 
-    void calcWinner()
+    std::pair<int, int> inputCell()
     {
+        int row, col;
+        while (true) {
+            std::cout << "Enter row (1-3) and column (1-3) for Player " << (now == PLAYER_X ? 'X' : 'O') << ": ";
+            std::cin >> row >> col;
+            if ((row < 1 || row > 3) ||
+                (col < 1 || col > 3)) {
+                    std::cout << "Invalid row or column value\n";
+                    continue;
+            } else if (board[row-1][col-1] != NONE) {
+                std::cout << "Cell (" << row << ", " << col << ") is already occupied\n";
+                continue;
+            } else break;
+        }
+        board[row-1][col-1] = now;
+        return { row-1, col-1 };
+    }
 
+    void calcWinner(std::pair<int, int> p)
+    {
+        int i = p.first,
+            j = p.second;
+        if (checkRow(i, j, now) || checkColumn(i, j, now)) {
+            winner = now;
+            now = NONE;
+            return;
+        }
+        if (checkDiagonals(now)) {
+            winner = now;
+            now = NONE;
+            return;
+        }
+        /* count for draw */
+        int cnt = 0;
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 3; ++j)
+                cnt += (board[i][j] != NONE);
+        if (cnt == 9)
+            winner = now = NONE;
     }
 
     bool hasEnded()
     {
-
+        return now == NONE;
     }
 
     void switchPlayer()
     {
-        if (now == PLAYER_X) now =  PLAYER_O;
-        else now = PLAYER_X;
+        now = (now == PLAYER_X) ? PLAYER_O : PLAYER_X;
     }
 
     void displayWinner()
     {
-        std::cout << "The winner is Player" << (now == PLAYER_X ? 'X' : 'O') << std::endl;
+        std::cout << "GAME OVER!\n";
+        if (winner == now && now == NONE) {
+            std::cout << "It's a draw\n";
+            return;
+        }
+        std::cout << "The winner is Player " << (winner == PLAYER_X ? 'X' : 'O') << std::endl;
     }
 
 private:
-    Player board[3][3];
-    Player now;
+    bool checkRow(int row, int col, Player player)
+    {
+        for (int j = 0; j < 3; ++j) {
+            if (board[row][j] != player) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkColumn(int row, int col, Player player)
+    {
+        for (int i = 0; i < 3; ++i) {
+            if (board[i][col] != player) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkDiagonals(Player player)
+    {
+        if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+            (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+                return true;
+        }
+        return false;
+    }
 };
 
 int main()
 {
-    Game game(
-        Game::inputPlayer("Enter player 1 (X/O): "),
-        Game::inputPlayer("Enter player 2 (X/O): "));
     while (true) {
-        game.inputCell();
-        game.calcWinner();
-        if (game.hasEnded()) break;
-        game.switchPlayer();
+        Game game(
+            Game::inputPlayer("Enter player 1 (X/O): "),
+            Game::inputPlayer("Enter player 2 (X/O): "));
+        game.displayBoard();
+        while (true) {
+            auto p = game.inputCell();
+            game.displayBoard();
+            game.calcWinner(p);
+            if (game.hasEnded()) break;
+            game.switchPlayer();
+        }
+        game.displayWinner();
+        std::cout << "\nEnter Y to replay, quit otherwise: ";
+        char ch; std::cin >> ch;
+        switch (ch) {
+            case 'y': case 'Y': continue;
+            default: exit(0);
+        }
     }
-    game.displayWinner();
     return 0;
 }
